79589458869d98097d5fc732fed81904ff4ab295
Roberto Boscolo <beto.boscolo@gmail.com>
2021-05-03 20:11:22 -0300

    Basic static components.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div className="text-primary">
+    <>
+      <NavBar />
+      <div className="container">
         <h1>hello</h1>
-    </div>
+      <DataTable/>  
+      </div>
+      <Footer/>
+    </>
   );
 }
 
